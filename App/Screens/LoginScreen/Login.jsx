8a634eb2a24223ed5163dccf115e043f43139587
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2024-03-19 08:32:27 -0400

    styling on font and login screen

--- App/Screens/LoginScreen/Login.jsx
+++ App/Screens/LoginScreen/Login.jsx
@@ -8,6 +8,11 @@ export default function Login() {
       <Image source={require('./../../../assets/images/login.png')}
         style={styles.loginImage}
       />
+      <View style={styles.subContainer}>
+        <Text style={{fontSize:27,color:Colors.WHITE, textAlign:'center'}}>
+            Let's Find Professional Cleaning Services!
+        </Text>
+      </View>
     </View>
   )
 }
@@ -19,5 +24,14 @@ const styles = StyleSheet.create({
         marginTop:70,
         borderWidth:4,
         borderColor:Colors.BLACK,
+        borderRadius:15
+    },
+    subContainer:{
+        width:'100%',
+        backgroundColor:Colors.PRIMARY,
+        height:'70%',
+        marginTop:-20,
+        borderTopLeftRadius:30,
+        borderTopRightRadius:30
     }
 })
